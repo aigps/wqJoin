db32143eef413273dcf64d04d64d89f486c297d5
ccq <ccq282738@163.com>
2013-09-21 11:50:48 +0800

    add gps report

---
+++ src/main/java/org/aigps/wq/WqConfig.java
@@ -0,0 +1,25 @@
+package org.aigps.wq;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WqConfig {
+	
+	@Value("${tmn.sys.id.rela.sql}")
+	private String tmnSysIdSql;
+
+	public String getTmnSysIdSql() {
+		return tmnSysIdSql;
+	}
+
+	public void setTmnSysIdSql(String tmnSysIdSql) {
+		this.tmnSysIdSql = tmnSysIdSql;
+	}
+	
+	
+	
+	
+	
+
+}
---
+++ src/main/java/org/aigps/wq/util/DistrictUtil.java
@@ -0,0 +1,17 @@
+package org.aigps.wq.util;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class DistrictUtil {
+	
+	private static final Log log = LogFactory.getLog(DistrictUtil.class);
+	
+	//根据经纬度获取所在行政区域
+	public static String getCityZcode(double lon,double lat)throws Exception{
+		
+		
+		return "";
+	}
+
+}
